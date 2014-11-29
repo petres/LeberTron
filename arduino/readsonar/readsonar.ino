09f19c58d97b7ad6cc680c357c6653953431cbf7
marcel <flushfm@yeara.net>
2014-11-29 22:31:29 +0100

    added option to read more distance sensors

--- arduino/readsonar/readsonar.ino
+++ arduino/readsonar/readsonar.ino
@@ -1,26 +1,50 @@
-#define trigPin 6
+#define trigPinA 6
+#define trigPinB 9
+
 #define echoPin 5
 
+int distanceA;
+int distanceB;
+
+unsigned long sensorATime;
+unsigned long sensorBTime;
+
 void setup() {                
-  Serial.begin (9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);    
+	Serial.begin(9600);
+	pinMode(trigPinA, OUTPUT);
+        pinMode(trigPinB, OUTPUT);
+	pinMode(echoPin, INPUT);
+  
+	digitalWrite(trigPinA, LOW);  // Added this line  
+        digitalWrite(trigPinB, LOW);
+	delayMicroseconds(2); // Added this line
 }
 
 void loop() {
-  long duration, distance;
-  digitalWrite(trigPin, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10); // Added this line
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
+	distanceA = getDistance(trigPinA);
+	distanceB = getDistance(trigPinB);
 
-  if (distance < 200 || distance > 0) {
-    Serial.println(distance);
-    //Serial.println(" cm");
-  }
-  delay(100);
-  //delay(10);
+	Serial.print("{\"A\": ");
+        Serial.print(distanceA);
+        Serial.print(", \"B\": ");
+        Serial.print(distanceB);
+        Serial.println("}; ");
+
+	delay(100);
+	//delay(10);
+}
+
+unsigned int getDistance(int trigPin) {
+	long duration, distance;
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10); // Added this line
+	digitalWrite(trigPin, LOW);
+	duration = pulseIn(echoPin, HIGH);
+	distance = (duration/2) / 29.1;
+
+	if (distance < 200 && distance > 0) {
+		return distance;
+	} else {
+		return -1;
+	}
 }
