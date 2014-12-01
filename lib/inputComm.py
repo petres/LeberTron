3c043f89a9c000711dbbf886b63212fa04ef1969
Lukas P <luk.puehringer@gmail.com>
2014-12-01 23:31:01 +0100

    inputComm now with oo

--- lib/inputComm.py
+++ lib/inputComm.py
@@ -13,90 +13,102 @@ keep track of states
 assess different states
 """
 
+class InputComm():
 
-SLIDING = True
-SLIDING_WINDOW_SIZE = 15
+    def __init__(self, serialPort='/dev/tty.usbserial-A9WFF5LH',
+        sliding = False, slidingWindowSize = 3, transform = False,
+        twoSensors = False, shootMin = 5, shootMax = 20):
 
+        self.exitFlag = False
+        self.slidingWindow = deque()
+        self.slidingWindowSize = slidingWindowSize
 
-# if there is an object between, we shoot
-SHOOT_MIN = 5
-SHOOT_MAX = 20
+        # if there is an object between, we shoot
+        self.shoot = False
+        self.shootMin = shootMin
+        self.shootMax = shootMax
 
-state = 0
-curr = 0
-currA = 0
-shoot = False
-shootDist = 0
+        # -1: left | 0: idle | 1: right
+        self.state = 0
+        self.position = 0
 
+        self.serialConn = False
+        self.readThread = False
 
-def inputRead():
-    global curr, state, currA, shoot
-
-    sliding_window = deque()
-
-    prevState = 0
-    while not exitFlag:
-        # CURRENT POSITION
-        # import ipdb; ipdb.set_trace()
-        line = serialConn.readline().rstrip('\r\n')
-        distances = line.split(" ")
-        logging.debug("LINE from Arduino: '%s'" % line)
-        try:
-            currA = float(distances[0]) / 90 * 2
-        except ValueError:
-            logging.warning("Could not read distance[0] from Arduino")
-            continue
-
-        logging.info("Received distances: " + str(distances))
+        self.transform = transform
+        self.twoSensors = twoSensors
 
         try:
-            shootDist = float(distances[1]) / 90 * 2
-        except ValueError:
-            logging.warning("Could not read distance[1] from Arduino")
-            shootDist = 0
+            self.serialConn = serial.Serial(serialPort, 9600)
+            self.readThread = threading.Thread(target=self.readInputCallback)
+            self.readThread.start()
+        except Exception, e:
+            raise e
 
-        if SLIDING:
-            if 0 < currA < 100:
-                sliding_window.append(currA)
-            if len(sliding_window) >= SLIDING_WINDOW_SIZE:
-                sliding_window.popleft()
-            if len(sliding_window) == 0:
-                curr = 0
-            else:
-                curr = sum(sliding_window) / len(sliding_window)
+    def close(self):
+        self.exitFlag = True
+        logging.info("INFO: Waiting for inputComm listen thread ... ")
+        self.readThread.join()
+        logging.info("DONE" + "\n")
+        self.serialConn.close()
 
-        if SHOOT_MIN <= shootDist <= SHOOT_MAX:
-            shoot = True
+
+    def doTheShoot(self, distance):
+        if self.shootMin <= distance <= self.shootMax:
+            self.shoot = True
         else:
-            shoot = False
+            self.shoot = False
+        
+    def doTheSlide(self, distance):
+        if 0 < distance < 100:
+            self.slidingWindow.append(distance)
+        if len(self.slidingWindow) >= self.slidingWindowSize:
+            self.slidingWindow.popleft()
+        if len(self.slidingWindow) == 0:
+            self.position = 0
+        else:
+            self.position = sum(self.slidingWindow) / len(self.slidingWindow)
 
-    serialConn.close()
+    def transformVal(self, serialInput):
+        return float(serialInput) / 90 * 2
 
+    def readInputCallback(self):
+        while not self.exitFlag:
 
-def start():
-    threadRead.start()
+            # CURRENT POSITION
+            # import ipdb; ipdb.set_trace()
+            try:
+                line = self.serialConn.readline().rstrip('\r\n')
+                logging.debug("LINE from Arduino: '%s'" % line)
 
-# Init Threads
-threadRead = threading.Thread(target=inputRead)
-exitFlag = 0  # Exit main
+                if self.twoSensors:
+                    vals = line.split(" ")
+                    distance = float(vals[0])
+                    shootDistance = float(vals[0])
+                    if self.transform:
+                        distance = self.transformVal(distance)
+                        shootDistance = self.transformVal(shootDistance)
+                else: 
+                    distance = float(line)
+                    if self.transform:
+                        distance = self.transform
 
-# Init Serial Connections
-serialConn = None
+                self.doTheSlide(distance)
 
-# def connect(serialPort = '/dev/ttyACM0'):
+                # distanceAndShoot = self.readShootAndDistance(line)
+                # self.doTheShoot(shootDistance)
 
+                print self.position
+            except Exception, e:
+                raise e
 
-def connect(serialPort='/dev/tty.usbserial-A9WFF5LH'):
-    global serialConn
-    serialConn = serial.Serial(serialPort, 9600)
 
 if __name__ == '__main__':
-    connect(serialPort='/dev/ttyACM0')
-    start()
+
+    s = InputComm()
+
     try:
         while True:
             time.sleep(0.2)
     except KeyboardInterrupt:
-        exitFlag = 1
-        threadRead.join()
-        # print "Ciao..."
+        s.close()
