ff5a8a67266f66438b20ce42d6b37f9fb08f5035
Jakob Unterwurzacher <jakobunt@gmail.com>
2014-12-03 20:01:45 +0100

    arduino onlyPosition: Add comment about actual trigger length

--- arduino/onlyPosition/src/controller.ino
+++ arduino/onlyPosition/src/controller.ino
@@ -11,9 +11,13 @@ void setup() {
 void loop() {
     unsigned long duration;
     delay(40);
+
+    // Actual pulse is 15us wide - good
+    // (measured on Leonardo)
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
+
     duration = pulseIn(echoPin, HIGH, 10000UL);
     Serial.println(duration);
     Serial.flush();
